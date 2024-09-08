44e680d1d92048481f0ea13fe4b92e7594a48bd3
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-09-08 09:42:23 +0100

    Made helper functions static methods. Added: clean NS chars method, public method to save file

--- ExtractTransform/extract_transform_species.py
+++ ExtractTransform/extract_transform_species.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pandas as pd
+import re
 import requests
 from pandas.api.types import CategoricalDtype
 
@@ -78,6 +79,12 @@ class ExtractSpecies:
         self.logger.info(f"Initial DataFrame shape: {self.dataframe.shape}")
 
         try:
+            self.dataframe = self._clean_non_standard_rows() # removing non-standard chars
+        except Exception as e:
+            self.logger.error(f"Error removing non-standard characters: {e}")
+            raise
+
+        try:
             self.dataframe = self._remove_false_data()
         except Exception as e:
             self.logger.error(f"Error removing false data: {e}")
@@ -99,16 +106,35 @@ class ExtractSpecies:
 
         self.logger.info(f"Subset DataFrame for category '{self.category}' shape: {self.dataframe.shape}")
 
+    def save_dataframe_to_csv(self, df, directory, filename):
+        """Saves the given DataFrame to a CSV file in the specified directory with error handling and logging."""
+        backup_dir = directory
+        try:
+            os.makedirs(backup_dir, exist_ok=True)
+            backup_file_path = os.path.join(backup_dir, filename)
+            df.to_csv(backup_file_path, index=True)
+
+            # Check if the file was created and log success
+            if os.path.exists(backup_file_path):
+                self.logger.info(f"Data successfully saved to {backup_file_path}")
+            else:
+                raise FileNotFoundError(f"File {backup_file_path} was not found after saving.")
+
+        except Exception as e:
+            self.logger.error(f"Error occurred while saving data: {e}")
+            raise
+
     def _setup_logger(self):
         """Configures a logger specific to the category."""
         log_dir = os.path.join(os.getcwd(), 'Logs')
         os.makedirs(log_dir, exist_ok=True)
+        formatted_category = self.category.lower().replace(" ", "_").replace("/", "_")
         logger = logging.getLogger(self.category)
         logger.setLevel(logging.INFO)
 
         # Check if the logger already has handlers to prevent adding duplicates
         if not logger.handlers:
-            log_file = os.path.join(log_dir, f'transformation_{self.category}.log')
+            log_file = os.path.join(log_dir, f'transformation_{formatted_category}.log')
             handler = logging.FileHandler(log_file, mode='a')
             formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
@@ -141,6 +167,46 @@ class ExtractSpecies:
         df = df[cols]
         return df
 
+    @staticmethod
+    def has_non_standard_chars(value):
+        if pd.isna(value):  # Check for NaN values
+            return False
+        # Check for non-standard characters using regex
+        return bool(re.search(r"[^a-zA-Z0-9\s,.()\-_'’ʻ`´]", str(value)))
+
+    @staticmethod
+    def clean_non_standard_chars(value):
+        if pd.isna(value):  # Check for NaN values
+            return value
+        value = re.sub(r"/", ", ", str(value))
+        value = re.sub(r";", ",", str(value))
+        value = re.sub(r"[^a-zA-Z0-9\s,.()\-&'’ʻ]", "", value)
+        return value
+
+    def _clean_non_standard_rows(self):
+        df = self.dataframe.copy()
+        columns = ['scientific_name', 'common_names']
+
+        # Apply the function column-wise to check for non-standard characters in each column
+        columns_with_issues = []
+        for column in columns:
+            if df[column].apply(self.has_non_standard_chars).any():
+                columns_with_issues.append(column)
+
+        # Filter rows where any of the columns have non-standard characters
+        df_with_non_standard_chars = df[df[columns_with_issues].apply(
+            lambda row: row.apply(self.has_non_standard_chars).any(), axis=1)].copy()
+        df_to_save = df_with_non_standard_chars[['category', 'scientific_name', 'common_names']]
+        self.logger.info(f"Found {len(df_to_save)} records with non-standard characters.")
+        self.save_dataframe_to_csv(df_to_save, "BackupData", "nonstandard_chars.csv")
+
+        # Apply the cleaning function in place
+        for column in columns_with_issues:
+            df_with_non_standard_chars[column] = df_with_non_standard_chars[column].apply(
+                self.clean_non_standard_chars)
+        self.dataframe = df
+        return df
+
     def _remove_false_data(self):
         """
         Filters out records where 'record_status' is not 'Approved' or 'In Review',
@@ -150,25 +216,7 @@ class ExtractSpecies:
         # Filter records where 'record_status' is not 'Approved' or 'In Review'
         non_standard_status_records = df[~df['record_status'].isin(['Approved', 'In Review'])].copy()
         self.logger.info(f"Found {len(non_standard_status_records)} records with non-standard 'record_status'.")
-
-        # Ensure the backup directory exists
-        backup_dir = "BackupData"
-        try:
-            os.makedirs(backup_dir, exist_ok=True)
-            backup_file_path = os.path.join(backup_dir, "incorrect_records.csv")
-
-            # Save the incorrect records to CSV
-            non_standard_status_records.to_csv(backup_file_path, index=True)
-
-            # Check if the file was created and log success
-            if os.path.exists(backup_file_path):
-                self.logger.info(f"Incorrect records successfully saved to {backup_file_path}")
-            else:
-                raise FileNotFoundError(f"File {backup_file_path} was not found after saving.")
-
-        except Exception as e:
-            self.logger.error(f"Error occurred while saving incorrect records: {e}")
-            raise  # Raise the error after logging it for visibility
+        self.save_dataframe_to_csv(non_standard_status_records, "BackupData", "incorrect_records.csv")
 
         # Indices that need correction based on 'non_standard_status_records'
         indices_to_shift = non_standard_status_records.index
@@ -190,6 +238,17 @@ class ExtractSpecies:
         self.dataframe = df
         return df
 
+    @staticmethod
+    def simplify_seasonality(value):
+        """
+        Simplifies the seasonality values to a single keyword based on priority.
+        """
+        priority_keywords = ['Winter', 'Summer', 'Breeder', 'Migratory', 'Resident', 'Vagrant']
+        for keyword in priority_keywords:
+            if keyword in value:
+                return keyword
+        return 'Unknown'
+
     def _transform_dataframe(self):
         """
         Transforms the entire DataFrame by setting categorical and boolean columns,
@@ -197,18 +256,7 @@ class ExtractSpecies:
         """
         df = self.dataframe.copy()
         df['seasonality'] = df['seasonality'].fillna('Unknown')
-        priority_keywords = ['Winter', 'Summer', 'Breeder', 'Migratory', 'Resident', 'Vagrant']
-
-        def simplify_seasonality(value):
-            """
-            Simplifies the seasonality values to a single keyword based on priority.
-            """
-            for keyword in priority_keywords:
-                if keyword in value:
-                    return keyword
-            return 'Unknown'
-
-        df['seasonality'] = df['seasonality'].apply(simplify_seasonality)
+        df['seasonality'] = df['seasonality'].apply(self.simplify_seasonality)
 
         # Define fill values and category orders
         fill_values = {
